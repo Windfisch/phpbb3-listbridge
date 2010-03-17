13a0097cc17f93ef3ee2a984e807bdead5c6987e
uckelman <uckelman@nomic.net>
2010-03-17 19:46:30 +0000

    Initial version.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6618 67b53d14-2c14-4ace-a08f-0dab2b34000c

---
+++ test/EmailMessageTest.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+
+class EmailMessageTest extends PHPUnit_Framework_TestCase {
+  
+  /** @test */ 
+  public function testGetSource() {
+  }
+
+  /** @test */ 
+  public function testGetPostId() {
+  }
+
+  /** @test */ 
+  public function testGetFrom() {
+  }
+
+  /** @test */ 
+  public function testGetSubject() {
+  }
+  
+  /** @test */ 
+  public function testGetMessageId() {
+  }
+
+  /** @test */ 
+  public function testGetInReplyTo() {
+  }
+
+  /** @test */ 
+  public function testGetReferences() {
+  }
+
+  /** @test */ 
+  public function testGetBody() {
+  }
+
+}
+
+?>

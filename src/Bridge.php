3b888ef4cddf97d2f51953b4dd33e1d35eb02930
uckelman <uckelman@nomic.net>
2010-03-23 22:38:14 +0000

    * Added getMessageId(), getPostId(). * Build posts table for tests.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6629 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/Bridge.php
+++ src/Bridge.php
@@ -8,31 +8,63 @@ class Bridge {
   public function __construct($db = FALSE) {
     $this->db = $db ? $db :
       new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB, DB_USER, DB_PASS);
+
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 
   public function getPostId($messageId) {
-    $sql = 'SELECT post_id FROM posts' .
-           'WHERE message_id = "' . $this->db->quote($message_id) . '"';
+    $sql = 'SELECT post_id FROM posts ' .
+           'WHERE message_id = ' . $this->db->quote($messageId);
 
     $result = $this->db->query($sql);
-    if (!$result) {
-      trigger_error("Unknown message id: $message_id", E_USER_ERROR);
-    }
 
-    // FIXME: what to do if more than one row is returned?
-    $row = $result->fetch(PDO::FETCH_ASSOC);
+    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
     $result->closeCursor();
 
-    return $row['post_id'];
+    switch (count($rows)) {
+    case 0:
+      trigger_error("Unknown message id: $messageId", E_USER_ERROR);
+      break;
+
+    case 1:
+      return $rows[0]['post_id'];
+
+    default:
+      trigger_error("Too many rows returned: $messageId", E_USER_ERROR);
+      break;
+    }   
+  }
+
+  public function getMessageId($postId) {
+    $sql = 'SELECT message_id FROM posts ' .
+           'WHERE post_id = ' . $this->db->quote($postId);
+
+    $result = $this->db->query($sql);
+
+    $rows = $result->fetchAll(PDO::FETCH_ASSOC);
+    $result->closeCursor();
+    
+    switch (count($rows)) {
+    case 0:
+      trigger_error("Unknown post id: $postId", E_USER_ERROR);
+      break;
+
+    case 1:
+      return $rows[0]['message_id'];
+
+    default:
+      trigger_error("Too many rows returned: $postId", E_USER_ERROR);
+      break;
+    }    
   }
 
   public function registerMessage($msg, $parentId) {
-    $sql = 'INSERT INTO posts' .
-           '(post_id, message_id, parent_message_id, references)' .
-           'VALUES('   . $msg->getPostId()                       . ', '
-                   '"' . $this->db->quote($msg->getMessageId())  . '", '
-                   '"' . $this->db->quote()                      . '", ' 
-                   '"' . $this->db->quote($msg->getReferences()) . '")'; 
+    $sql = 'INSERT INTO posts ' .
+           '(post_id, message_id, in_reply_to, refs) ' .
+           'VALUES (' . $msg->getPostId()                       . ', '
+                      . $this->db->quote($msg->getMessageId())  . ', '
+                      . $this->db->quote()                      . ', '
+                      . $this->db->quote($msg->getReferences()) . ')'; 
 
     $count = $this->db->exec($sql);
 
@@ -44,8 +76,8 @@ class Bridge {
   }
 
   public function getDefaultForumId($list) {
-    $sql = 'SELECT forum_id FROM bridge' .
-           'WHERE list_name = "' . $this->db->quote($list) . '"';
+    $sql = 'SELECT forum_id FROM bridge ' .
+           'WHERE list_name = ' . $this->db->quote($list);
 
     $result = $this->db->query($sql);
     if (!$result) {
--- test/BridgeTest.php
+++ test/BridgeTest.php
@@ -5,18 +5,68 @@ require_once('src/Bridge.php');
 
 class BridgeTest extends PHPUnit_Framework_TestCase {
 
+  protected $db;
+
+  protected function setUp() {
+    $this->db = new PDO('mysql:host=localhost;dbname=test');
+    $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $this->db->exec('DROP TABLE IF EXISTS posts');
+
+    $this->db->exec(
+      'CREATE TABLE posts (' .
+      'post_id MEDIUMINT UNSIGNED NOT NULL, ' .
+      'message_id VARCHAR(255) NOT NULL, ' .
+      'in_reply_to VARCHAR(255), ' .
+      'refs BLOB, ' .
+      'PRIMARY KEY (post_id), ' .
+      'UNIQUE KEY (message_id))'
+    );
+
+    $this->db->exec(
+      'INSERT INTO posts (post_id, message_id, in_reply_to, refs) ' .
+      'VALUES (' .
+        '1, ' .
+        '"<20100302094228.33F0310091@charybdis.ellipsis.cx>", ' .
+        '"<1267473003.m2f.17543@www.vassalengine.org>", ' .
+        '"<1267171317.m2f.17507@www.vassalengine.org> <1267473003.m2f.17543@www.vassalengine.org>"' .
+      ')'
+    );
+  }
+
+  protected function tearDown() {
+    $this->db = null;
+  }
+
   /**
    * @dataProvider providerGetPostId
    */
   public function testGetPostId($message_id, $expected, $ex) {
-    $bridge = new Bridge();
     if ($ex) $this->setExpectedException($ex);
+    $bridge = new Bridge($this->db);
     $this->assertEquals($expected, $bridge->getPostId($message_id));
   }
 
   public function providerGetPostId() {
     return array(
       array('bogus', null, 'PHPUnit_Framework_Error'), 
+      array('<20100302094228.33F0310091@charybdis.ellipsis.cx>', 1, null),
+    );
+  }
+
+  /**
+   * @dataProvider providerGetMessageId
+   */
+  public function testGetMessageId($post_id, $expected, $ex) {
+    if ($ex) $this->setExpectedException($ex);
+    $bridge = new Bridge($this->db);
+    $this->assertEquals($expected, $bridge->getMessageId($post_id));
+  }
+
+  public function providerGetMessageId() {
+    return array(
+      array('bogus', null, 'PHPUnit_Framework_Error'), 
+      array(1, '<20100302094228.33F0310091@charybdis.ellipsis.cx>', null),
     );
   }
 
@@ -25,7 +75,7 @@ class BridgeTest extends PHPUnit_Framework_TestCase {
    */
   public function testRegisterMessage($msg, $expected, $ex) {
     $this->markTestIncomplete();
-#    $bridge = new Bridge();
+#    $bridge = new Bridge($this->db);
 #    if ($ex) $this->setExpectedException($ex);
 #    $this->assertEquals($expected, $bridge->registerMessage($msg));
   }
@@ -39,9 +89,10 @@ class BridgeTest extends PHPUnit_Framework_TestCase {
    * @dataProvider providerGetDefaultForumId
    */
   public function testGetDefaultForumId($list, $expected, $ex) {
-    $bridge = new Bridge();
-    if ($ex) $this->setExpectedException($ex);
-    $this->assertEquals($expected, $bridge->getDefaultForumId($list));
+    $this->markTestIncomplete();
+#    if ($ex) $this->setExpectedException($ex);
+#    $bridge = new Bridge($this->db);
+#    $this->assertEquals($expected, $bridge->getDefaultForumId($list));
   }
 
   public function providerGetDefaultForumId() {
@@ -50,8 +101,6 @@ class BridgeTest extends PHPUnit_Framework_TestCase {
 #      array('messages@forums.vassalengine.org', 2, null),
     );
   }
-
-
 }
 
 ?>

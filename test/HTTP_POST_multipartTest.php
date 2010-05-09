9e545c4d98b6a20bcaa762b70a2a9af46ade5944
uckelman <uckelman@nomic.net>
2010-05-09 15:42:17 +0000

    Adjustments to test.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@6815 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- test/HTTP_POST_multipartTest.php
+++ test/HTTP_POST_multipartTest.php
@@ -5,24 +5,19 @@ require_once('src/HTTP_POST_multipart.php');
 
 class HTTP_POST_multipartTest extends PHPUnit_Framework_TestCase {
 
-  protected static $_class;
-
-  public static function setUpBeforeClass() {
-    # Set all methods to be public so we can test them
-    self::$_class = new ReflectionClass('HTTP_POST_multipart');
-    foreach (self::$_class->getMethods() as $method) {
-      $method->setAccessible(true);
-    }
-  }
-
   /**
    * @dataProvider providerBuildDataPart
    */
   public function testBuildDataPart($name, $data, $expected, $ex) {
     if ($ex) $this->setExpectedException($ex);
+
+    $class = new ReflectionClass('HTTP_POST_multipart');
+    $method = $class->getMethod('bulidDataPart');
+    $method->setAccessible(true);
+
     $this->assertEquals(
       $expected,
-      self::$_class->getMethod('bulidDataPart', array($name, $data))
+      $method->invokeArgs(null, array($name, $data))
     );
   }
 

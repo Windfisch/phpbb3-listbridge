b3481ffa2097b304291c13cabd00b229695f643c
uckelman <uckelman@nomic.net>
2010-07-27 20:47:16 +0000

    Added is_ascii().
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7041 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/Util.php
+++ src/Util.php
@@ -8,11 +8,9 @@ function build_message_id($postId, $editId, $time, $forumHost) {
   return "<$time.$postId.$editId.bridge@$forumHost>";
 }
 
-/*
 function is_ascii($string) {
-  return !preg_match('/[^\x00-\x7F]/S', $str);
+  return !preg_match('/[^[:ascii:]]/', $str);
 }
-*/
 
 function utf8_quote($string) {
   return '=?UTF-8?B?' . base64_encode($string) . '?=';

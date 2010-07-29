f7f0e15f2dfe5e068d5a44376d74403ef21c0cd7
uckelman <uckelman@nomic.net>
2010-07-29 13:23:57 +0000

    * Added license. * Added Id tag.
    git-svn-id: https://vassalengine.svn.sourceforge.net/svnroot/vassalengine/site-src/trunk@7051 67b53d14-2c14-4ace-a08f-0dab2b34000c

--- src/forum_post_send.php
+++ src/forum_post_send.php
@@ -1,6 +1,26 @@
 <?php
 
 #
+# $Id$
+#
+# forum-list bridge 
+# Copyright (C) 2010 Joel Uckelman
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
 # Usage: In posting.php, following submit_post():
 # 
 # require_once('/home/uckelman/site-src/bridge/src/forum_post_send.php'); 
